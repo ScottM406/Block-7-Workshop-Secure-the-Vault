7a0d4854f2617c6432b6ca67010f934371bbb679
Scott <scottyjmt13@gmail.com>
2024-10-09 11:06:41 -0600

    initial commit - completed workshop

---
+++ index.js
@@ -0,0 +1,11 @@
+let firstNumber = 88; // math for first combination number based on this number
+let secondNumber = 1090; // math for second combination number based on this number
+let thirdNumber = 1; // math for third combination number based on this number
+
+let firstCombinationNumber =  firstNumber * 0.5 - 35 + 1; // first number in alert combination
+let secondCombinationNumber = secondNumber - 900 - 170 + 20; // secpmd number in alert combination
+let thirdCombinationNumber = thirdNumber * 78 / 2;
+
+alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+  ${firstCombinationNumber } - ${secondCombinationNumber } - ${thirdCombinationNumber}`
+)
